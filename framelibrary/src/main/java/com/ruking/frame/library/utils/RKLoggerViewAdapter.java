ef1a50e6f4131a7b5382d58be7d2aa3f00375b20
史伟成 <swc@fengjiangit.com>
2020-07-28 13:47:05 +0800

    添加日志

---
+++ framelibrary/src/main/java/com/ruking/frame/library/utils/RKLoggerViewAdapter.java
@@ -0,0 +1,82 @@
+package com.ruking.frame.library.utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ruking.frame.library.R;
+import com.ruking.frame.library.bean.LoggerTag;
+
+/**
+ * @author Ruking.Cheng
+ * @descrilbe 日志
+ * @email 495095492@qq.com
+ * @tel 18075121944
+ * @date on 2020-07-28 13:20
+ */
+public class RKLoggerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context mContext;
+
+    public RKLoggerViewAdapter(@NonNull Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.rk_dialog_checkbox, parent, false);
+        return new DescViewHolder(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        final DescViewHolder descHolder = (DescViewHolder) holder;
+        LoggerTag item = Logger.getLoggerTags().get(position);
+        descHolder.mItemImag.setVisibility(View.GONE);
+        descHolder.mItemText.setText(item.getTag() + "：" + item.getMsg());
+        switch (item.getPriority()) {
+            case 4:
+                descHolder.mItemText.setTextColor(Color.parseColor("#F44336"));
+                break;
+            case 3:
+                descHolder.mItemText.setTextColor(Color.parseColor("#FF3B4B"));
+                break;
+            case 2:
+                descHolder.mItemText.setTextColor(Color.parseColor("#FFEB3B"));
+                break;
+            case 1:
+                descHolder.mItemText.setTextColor(Color.parseColor("#2196F3"));
+                break;
+            default:
+                descHolder.mItemText.setTextColor(Color.parseColor("#FFFFFF"));
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return Logger.getLoggerTags().size();
+    }
+
+
+    private static class DescViewHolder extends RecyclerView.ViewHolder {
+        private ImageView mItemImag;
+        private TextView mItemText;
+
+        @SuppressLint("CutPasteId")
+        DescViewHolder(View itemView) {
+            super(itemView);
+            mItemImag = itemView.findViewById(R.id.item_imag);
+            mItemText = itemView.findViewById(R.id.item_text);
+        }
+    }
+
+}
--- framelibrary/src/main/java/com/ruking/frame/library/utils/RKLoggerViewUtil.java
+++ framelibrary/src/main/java/com/ruking/frame/library/utils/RKLoggerViewUtil.java
@@ -5,14 +5,11 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
-import android.os.Handler;
-import android.os.Message;
+import android.support.v7.widget.LinearLayoutManager;
 import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 import com.ruking.frame.library.R;
 import com.ruking.frame.library.bean.LoggerTag;
@@ -20,6 +17,7 @@ import com.ruking.frame.library.view.ToastUtil;
 import com.ruking.frame.library.view.animation.RKAnimationButton;
 import com.zhy.autolayout.AutoFrameLayout;
 import com.zhy.autolayout.AutoLinearLayout;
+import com.zhy.autolayout.AutoRecyclerView;
 
 import java.util.List;
 
@@ -31,43 +29,34 @@ import java.util.List;
  * @date on 2020-07-27 17:01
  */
 public class RKLoggerViewUtil {
-    private Activity mActivity;
-    private TextView mFrameTagText;
-    private ScrollView mFrameTagScroll;
+    private AutoRecyclerView mFrameTagScroll;
     private AutoLinearLayout mFrameTagLayout;
     private LoggerAdapter mLoggerAdapter;
-
-    @SuppressLint("HandlerLeak")
-    private Handler eventProcessingHandler = new Handler() {
-        public void handleMessage(Message msg) {
-            mActivity.runOnUiThread(() -> {
-                List<LoggerTag> loggerTags = Logger.getLoggerTags();
-                StringBuilder sb = new StringBuilder();
-                for (int i = 0; i < loggerTags.size(); i++) {
-                    LoggerTag loggerTag = loggerTags.get(i);
-                    sb.append(loggerTag.getTag()).append("：").append(loggerTag.getMsg());
-                    if (i != loggerTags.size() - 1) {
-                        sb.append("\n");
-                    }
-                }
-                mFrameTagText.setText(sb.toString());
-                mFrameTagScroll.scrollTo(0, mFrameTagText.getBottom());
-            });
-        }
-    };
+    private RKLoggerViewAdapter mAdapter;
 
     @SuppressLint("ClickableViewAccessibility")
     public RKLoggerViewUtil(Activity activity) {
-        mActivity = activity;
         RKAnimationButton frameTagBut01 = activity.findViewById(R.id.frame_tag_but01);
         RKAnimationButton frameTagBut02 = activity.findViewById(R.id.frame_tag_but02);
         AutoFrameLayout frameTagTipLayout = activity.findViewById(R.id.frame_tag_tip_layout);
-        mFrameTagText = activity.findViewById(R.id.frame_tag_text);
         mFrameTagScroll = activity.findViewById(R.id.frame_tag_scroll);
         mFrameTagLayout = activity.findViewById(R.id.frame_tag_layout);
+        mFrameTagScroll.setLayoutManager(new LinearLayoutManager(activity));
+        mFrameTagScroll.getItemAnimator().setChangeDuration(0);
+        mAdapter = new RKLoggerViewAdapter(activity);
+        mFrameTagScroll.setAdapter(mAdapter);
         frameTagBut01.setOnClickListener(v -> Logger.clearLoggerTagList());
         frameTagBut02.setOnClickListener(v -> {
-            String text = mFrameTagText.getText().toString().trim();
+            List<LoggerTag> loggerTags = Logger.getLoggerTags();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < loggerTags.size(); i++) {
+                LoggerTag loggerTag = loggerTags.get(i);
+                sb.append(loggerTag.getTag()).append("：").append(loggerTag.getMsg());
+                if (i != loggerTags.size() - 1) {
+                    sb.append("\n");
+                }
+            }
+            String text = sb.toString();
             if (!TextUtils.isEmpty(text)) {
                 ClipboardManager cm = (ClipboardManager) activity.getSystemService(Context.CLIPBOARD_SERVICE);
                 if (cm != null) {
@@ -88,10 +77,10 @@ public class RKLoggerViewUtil {
             }
             return true;
         });
-        mLoggerAdapter = loggerTags -> {
-            eventProcessingHandler.removeMessages(1);
-            eventProcessingHandler.sendEmptyMessageDelayed(1, 300);
-        };
+        mLoggerAdapter = loggerTags -> activity.runOnUiThread(() -> {
+            mAdapter.notifyDataSetChanged();
+            mFrameTagScroll.scrollToPosition(mAdapter.getItemCount() - 1);
+        });
     }
 
     public void onResume() {
